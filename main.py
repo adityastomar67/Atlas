1d380fc616b023b6e2d0cb1f9e5b27bf2beb4358
Aditya Singh Tomar <adityastomar12@gmail.com>
2022-04-18 19:39:13 +0530

    First Commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import datetime
+import json
+import os
+import subprocess
+import time
+import webbrowser
+
+import pyttsx3
+import requests
+import speech_recognition as sr
+import wikipedia
+import wolframalpha
+from ecapture import ecapture as ec
