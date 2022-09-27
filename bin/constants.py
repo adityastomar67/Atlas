9c183caacce5ff683cf6d00854e3592bb174ae30
Aishwarya Shrivastava <shrivastavaaishwarya711@gmail.com>
2022-09-27 12:19:44 +0530

    new files

---
+++ bin/constants.py
@@ -0,0 +1,13 @@
+from xml.etree.ElementTree import VERSION
+
+
+username = input("What's your Name?")
+
+CREATOR = "Aditya Singh Tomar"
+USER_PATH = f"C:\\Users\\{username}\\AppData\\Local"
+VERSION = "1.0"
+
+# Socials
+GITHUB_USERNAME = "adityastomar67"
+TWITTER_USERNAME = "singhhhx"
+INSTAGRAM_USERNAME = "singhhhx"
